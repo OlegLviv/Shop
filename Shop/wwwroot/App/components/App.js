2182f636034207b3d0851f70c402c87c3a3a14b0
Oleh <olehspidey@gmail.com>
2018-03-17 02:35:52 +0200

    commit

---
+++ Shop/wwwroot/App/components/App.js
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+import { Layout } from './Layout';
+import { hot } from 'react-hot-loader'
+
+const App = () => <Layout />;
+
+export default hot(module)(App)
---
+++ Shop/wwwroot/App/components/Layout.js
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+
+export const Layout = () => {
+    return (
+        <div>layout</div>
+    );
+}
