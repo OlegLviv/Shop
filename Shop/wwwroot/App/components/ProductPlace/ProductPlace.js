9d96b26f01c58029f647cb1f5e05641453394205
Oleh <olehspidey@gmail.com>
2018-03-24 13:48:11 +0200

    added product card

--- Shop/wwwroot/App/components/ProductPlace/ProductPlace.js
+++ Shop/wwwroot/App/components/ProductPlace/ProductPlace.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import {apiWithoutRedirect} from "../../services/api";
 import {getProductUrl} from "../../services/urls/productUrls";
+import ProductCard from "./ProductCard/ProductCard";
 
 const getCategory = (props) => props.match.params.category;
 const getSubCategory = (props) => props.match.params.subCategory;
 
 class ProductPlace extends React.Component {
-	render() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: []
+		}
+	}
+
+	componentDidMount() {
 		const prodUrl = getProductUrl(getCategory(this.props), getSubCategory(this.props));
-		console.log(prodUrl);
 		apiWithoutRedirect()
 			.get(prodUrl)
 			.then(resp => {
-				console.log(resp);
-			});
+				this.setState({products: resp.data});
+			})
+			.catch(err => {
+				console.log(err.response);
+			})
+	}
+
+	render() {
 		return (
-			<div>
-				<div>{getCategory(this.props)}</div>
-				<div>{getSubCategory(this.props)}</div>
+			<div className="container-fluid">
+				<div className="row">
+					{this.state.products.map(item => {
+						return (
+							<div className="col-4">
+								<ProductCard product={item}/>
+							</div>
+						)
+					})}
+				</div>
 			</div>
 		);
 	}
