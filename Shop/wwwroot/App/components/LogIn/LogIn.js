5d6a1891588f27a7397cbf0bf8dc2d5c217c3f76
Oleh <olehspidey@gmail.com>
2018-06-16 21:40:24 +0300

    added spinner to Login component

--- Shop/wwwroot/App/components/LogIn/LogIn.js
+++ Shop/wwwroot/App/components/LogIn/LogIn.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './LogIn.scss'
 import {logInToken} from "../../services/authService";
 import {normalizeLogInResponse} from "../../utils/responseUtils";
+import {Spinner} from "../Spinner/Spinner";
 
 class LogIn extends React.Component {
 	constructor(props) {
@@ -12,9 +13,12 @@ class LogIn extends React.Component {
 			isValidUserName: true,
 			isValidPassword: true,
 			userNameError: '',
-			passwordError: ''
+			passwordError: '',
+			loading: false
 		}
 	}
+
+	trySetLoading = () => !this.state.loading && this.setState({loading: true});
 
 	onChangeUserName = ({target}) => this.setState({userName: target.value});
 
@@ -22,8 +26,13 @@ class LogIn extends React.Component {
 
 	onLogin = e => {
 		e.preventDefault();
-		logInToken(this.state.userName, this.state.password)
+
+		this.trySetLoading();
+
+		logInToken(this.state.userName, this.state.password, () => this.setState({loading: false}))
 			.catch(err => {
+				this.setState({loading: false});
+
 				normalizeLogInResponse(err.response, userName => {
 						this.setState({
 							userNameError: userName,
@@ -36,7 +45,7 @@ class LogIn extends React.Component {
 							isValidPassword: false
 						})
 					});
-			})
+			});
 	};
 
 	renderError = text => <small className="form-text text-muted invalid-small">{text}</small>;
@@ -48,32 +57,35 @@ class LogIn extends React.Component {
 					<h3>Вхід</h3>
 					<hr/>
 				</div>
-				<form onSubmit={this.onLogin} method="">
-					<div className="form-group">
-						<label htmlFor="Email">Email або логін</label>
-						<input type="text" className={`form-control ${!this.state.isValidUserName && 'invalid-input'}`}
-							   id="Email"
-							   aria-describedby="emailHelp" placeholder="Введіть email або логін"
-							   value={this.state.userName}
-							   onChange={this.onChangeUserName}/>
-						{!this.state.isValidUserName && this.renderError(this.state.userNameError)}
-					</div>
-					<div className="form-group">
-						<label htmlFor="password">Пароль</label>
-						<input type="password"
-							   className={`form-control ${!this.state.isValidPassword && 'invalid-input'}`}
-							   id="password"
-							   placeholder="Введіть пароль..."
-							   value={this.state.password}
-							   onChange={this.onChangePassword}/>
-						{!this.state.isValidPassword && this.renderError(this.state.passwordError)}
-					</div>
-					<div className="form-group form-check">
-						<input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-						<label className="form-check-label" htmlFor="exampleCheck1">Запам'ятати</label>
-					</div>
-					<button type="submit" className="btn btn-primary">Увійти</button>
-				</form>
+				{
+					!this.state.loading ? <form onSubmit={this.onLogin} method="">
+						<div className="form-group">
+							<label htmlFor="Email">Email або логін</label>
+							<input type="text"
+								   className={`form-control ${!this.state.isValidUserName && 'invalid-input'}`}
+								   id="Email"
+								   aria-describedby="emailHelp" placeholder="Введіть email або логін"
+								   value={this.state.userName}
+								   onChange={this.onChangeUserName}/>
+							{!this.state.isValidUserName && this.renderError(this.state.userNameError)}
+						</div>
+						<div className="form-group">
+							<label htmlFor="password">Пароль</label>
+							<input type="password"
+								   className={`form-control ${!this.state.isValidPassword && 'invalid-input'}`}
+								   id="password"
+								   placeholder="Введіть пароль..."
+								   value={this.state.password}
+								   onChange={this.onChangePassword}/>
+							{!this.state.isValidPassword && this.renderError(this.state.passwordError)}
+						</div>
+						<div className="form-group form-check">
+							<input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+							<label className="form-check-label" htmlFor="exampleCheck1">Запам'ятати</label>
+						</div>
+						<button type="submit" className="btn btn-primary">Увійти</button>
+					</form> : <Spinner/>
+				}
 			</div>
 		);
 	}
