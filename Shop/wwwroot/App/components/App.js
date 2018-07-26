bf8ffa09d04fa8b86bef27ded89e8d8d2eac8b3c
Oleh <olehspidey@gmail.com>
2018-07-26 19:13:18 +0300

    added document title

--- Shop/wwwroot/App/components/App.js
+++ Shop/wwwroot/App/components/App.js
@@ -1,5 +1,11 @@
 ﻿import React from 'react';
 import Layout from './Layout';
 import '../main.scss';
+import DocumentTitle from 'react-document-title';
+import {SITE_NAME} from "../constants/siteConstants";
 
-export const App = () => <Layout/>;
+export const App = () => (
+	<DocumentTitle title={SITE_NAME}>
+		<Layout/>
+	</DocumentTitle>
+);
